85cea0f900af8e7fc8fe17a4629d5387bdfdbd79
Towfiqul Islam <tawfiq.inflack@bkash.com>
2020-02-06 13:59:43 +0600

    Unit Test controller WelcomeControllerTest is complete

--- web-thymeleaf/src/test/java/com/javaproject/WelcomeControllerTest.java
+++ web-thymeleaf/src/test/java/com/javaproject/WelcomeControllerTest.java
@@ -1,1 +1,38 @@
 package com.javaproject;
+
+import com.javaproject.controller.WelcomeController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = WelcomeController.class)
+public class WelcomeControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    List<String> expectedList = Arrays.asList("a", "b", "c", "d", "e", "f", "g");
+
+    @Test
+    public void main() throws Exception {
+        ResultActions resultActions = mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("welcome"))
+                .andExpect(model().attribute("message", equalTo("Java-Project")))
+                .andExpect(model().attribute("tasks", is(expectedList)))
+                .andExpect(content().string(containsString("Hello, Java-Project")));
+    }
+}
